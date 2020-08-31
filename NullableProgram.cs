b3cb557b909dd24bb84d4b8d90b8e726df11e8aa
Gane Kameswar Rao <kameswar08gane@gmail.com>
2020-08-31 17:15:25 +0530

    added nullable concept implmentation

---
+++ NullableProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NullableDemo
+{
+    class NullableProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int? num1 = 20;
+            int? num2 = null;
+
+            double? num3 = 3.14;
+            double? num4 = new double?();
+
+            bool? boolval = new bool?();
+
+            Console.WriteLine("num1: {0},num2: {1}, Num3: {2}, Num4: {3}",num1,num2,num3,num4);
+            Console.WriteLine("BoolValue",boolval);
+
+            Console.ReadLine();
+        }
+    }
+}
